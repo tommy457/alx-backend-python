72d56705318cf66f1a94b45da7bdd66d1f70092d
tommy457 <boukhalfaml1011@gmail.com>
2024-03-14 18:46:22 +0000

    task 1+2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for the concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenated string of str1 and str2"""
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for the floor function"""
+
+
+def floor(n: float) -> int:
+    """ Returns the floor value of the input float n """
+    return int(n)
