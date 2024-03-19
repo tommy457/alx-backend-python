de261665464c18701657514eabc0b184c39a5e7e
tommy457 <boukhalfaml1011@gmail.com>
2024-03-19 13:45:40 +0000

    change type from Iterable to List

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,12 +2,12 @@
 """
 Module for the async_comprehension function.
 """
-from typing import Iterable
+from typing import List 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Iterable[float]:
+async def async_comprehension() -> List[float]:
     """coroutine will collect 10 random numbers,
     using an async comprehensing over async_generator and return them.
     """
