8e1cf4ececc5d3b34deedd0aeb87effbe1a28bf8
tommt457 <boukhalfaml1011@gmail.com>
2024-04-05 16:48:04 +0000

    feat: added Parameterize a unit test, Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@ Module for Unittests.
 """
 from parameterized import parameterized
 import unittest
-from utils import access_nested_map
+from unittest.mock import patch
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -16,9 +17,57 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"nested_map": {"a": {"b": 2}}, "path": ("a", "b")}, 2),
     ])
     def test_access_nested_map(self, input_data, expected_output):
-        """ tests for the access_nested_map method. """
+        """tests for the access_nested_map returns correct expected_output."""
         self.assertEqual(
             access_nested_map(**input_data), expected_output)
+
+    @parameterized.expand([
+        ({"nested_map": {}, "path": ("a",)}, "'a'"),
+        ({"nested_map": {"a": 1}, "path": ("a", "b")}, "'b'"),
+    ])
+    def test_access_nested_map_exception(self, input_data, error_msg):
+        """ test that a KeyError is raised for the input_data. """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(**input_data)
+        self.assertEqual(str(e.exception), error_msg)
+
+
+class TestGetJson(unittest.TestCase):
+    """ Unittests """
+    @parameterized.expand([
+        ({"test_url": "http://example.com", "test_payload": True},),
+        ({"test_url": "http://holberton.io", "test_payload": False},),
+    ])
+    @patch("requests.get", )
+    def test_get_json(self, input_data, mock_get):
+        """ test that get_json returns the expected result. """
+        mock_get.return_value.json.return_value = input_data["test_payload"]
+
+        data = get_json(input_data["test_url"])
+
+        mock_get.assert_called_once_with(input_data["test_url"])
+        self.assertEqual(data, input_data["test_payload"])
+
+
+class TestMemoize(unittest.TestCase):
+    """ Unittests """
+    def test_memoize(self):
+        """ test the correct result is returned. """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method") as mocked_method:
+            obj = TestClass()
+            obj.a_property()
+            obj.a_property()
+
+        mocked_method.assert_called_once()
 
 
 if __name__ == "__main__":
