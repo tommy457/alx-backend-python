01878e574bd9dc69f2d57ac7eebde3385a2d7ece
tommt457 <boukhalfaml1011@gmail.com>
2024-04-06 15:53:11 +0000

    test: added tests for the client module

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Unittests for the  client module.
+"""
+from client import GithubOrgClient
+from parameterized import parameterized
+import unittest
+from unittest.mock import patch, PropertyMock
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Tests for Githib org client. """
+
+    @parameterized.expand([
+        ("google"), ("abc")
+    ])
+    @patch("client.get_json")
+    def test_org(self, input_data, mock_get_json):
+        """ Test that `org` method returns the correct value. """
+        obj = GithubOrgClient(input_data)
+        obj.org
+        test_url = obj.ORG_URL.format(org=input_data)
+
+        mock_get_json.assert_called_once_with(test_url)
+
+    @parameterized.expand([
+        ({"repos_url": "google"}, "google"),
+        ({"repos_url": "abc"}, "abc"),
+    ])
+    def test_public_repos_url(self, payload, expected_output):
+        """Test that `_public_repos_url` method returns the expected value"""
+        with patch("client.GithubOrgClient.org",
+                   new_callable=PropertyMock) as mocked_property:
+            mocked_property.return_value = payload
+            obj = GithubOrgClient("test")
+
+            self.assertEqual(obj._public_repos_url, expected_output)
+
+    @parameterized.expand([
+        ([{"name": "google"}, {"name": "abc"}], ["google", "abc"])
+    ])
+    @patch("client.get_json")
+    def test_public_repos(self, payload, expected_output, mock_get_json):
+        """ Test for the `public_repos` method """
+        mock_get_json.return_value = payload
+
+        with patch("client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock) as mocked_property:
+            mocked_property.return_value = "repos_url"
+
+            obj = GithubOrgClient("test")
+            self.assertEqual(obj.public_repos(), expected_output)
+
+            mock_get_json.assert_called_once()
+            mocked_property.assert_called_once()
+
+
+if __name__ == "__main__":
+    unittest.main()
