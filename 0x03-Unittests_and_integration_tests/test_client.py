e11a24cd82535570353e5e280c8ded3f3c8c2c8e
tommt457 <boukhalfaml1011@gmail.com>
2024-04-06 16:26:12 +0000

    test: added test for has_license method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -16,7 +16,7 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch("client.get_json")
     def test_org(self, input_data, mock_get_json):
-        """ Test that `org` method returns the correct value. """
+        """ Test that `org` method returns the expected  correct value. """
         obj = GithubOrgClient(input_data)
         obj.org
         test_url = obj.ORG_URL.format(org=input_data)
@@ -41,9 +41,8 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch("client.get_json")
     def test_public_repos(self, payload, expected_output, mock_get_json):
-        """ Test for the `public_repos` method """
+        """ Test for the `public_repos` method returns the expected value. """
         mock_get_json.return_value = payload
-
         with patch("client.GithubOrgClient._public_repos_url",
                    new_callable=PropertyMock) as mocked_property:
             mocked_property.return_value = "repos_url"
@@ -53,6 +52,18 @@ class TestGithubOrgClient(unittest.TestCase):
 
             mock_get_json.assert_called_once()
             mocked_property.assert_called_once()
+
+    @parameterized.expand([
+        ({"repo": {"license": {"key": "my_license"}},
+          "license_key": "my_license"}, True),
+        ({"repo": {"license": {"key": "other_license"}},
+          "license_key": "my_license"}, False)
+    ])
+    def test_has_license(self, input_data, expected_output):
+        """ Test for the `has_license` method returns the expected  value. """
+        obj = GithubOrgClient("test")
+
+        self.assertEqual(obj.has_license(**input_data), expected_output)
 
 
 if __name__ == "__main__":
